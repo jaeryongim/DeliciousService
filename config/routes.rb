b0e4e9219e1044c170e0cba149af7789a35c56f2
steveim1201 <jr.im@stripes.co.kr>
2016-05-24 17:40:19 +0900

    add post modfiy func

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Service::Application.routes.draw do
   get "foods/write"
   post "foods/write_complete"
 
-  get "foods/edit"
-  get "foods/edit_complete"
+  get 'foods/edit/:id' => 'foods#edit'
+  post "foods/edit_complete"
+
   get "foods/delete_complete"
 
 
